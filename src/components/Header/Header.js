9a78f3955ba0ea73e60677ce1251b89723d7074d
orun sunny <sunny.mukut@gmail.com>
2022-09-26 23:28:53 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import logo from '../../images/Logo.svg';
-import './Header.css'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-
-const Header = () => {
-    return (
-        <div>
-          <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Orders</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/about">About</a>
-            </div>
-        </nav>
-        </div>
-    );
-};
-
-export default Header;
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+          <h2>Price</h2>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
