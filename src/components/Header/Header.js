c8acff6ffee5cb56f72db51583fa720f57775b9b
orun sunny <sunny.mukut@gmail.com>
2022-09-27 23:44:39 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,19 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
-export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          {/* <Button color="inherit">Login</Button> */}
-          <h2>Price</h2>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className="shop-container">
+            <div className="products-container">
+
+                <h3>This is for product</h3>
+            </div>
+            <div className="cart-container">
+                <h3>this is for order summary </h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
