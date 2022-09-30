200d4feebd1a7c943f3a88bb7120af87bc99d82f
orun sunny <sunny.mukut@gmail.com>
2022-09-30 11:56:57 +0600

    addedCArt

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
 
-const Product = (props) => {
-    const{name,img,seller,price,ratings}= props.product;
+const Product = ({product, handleAddToCart}) => {
+    // const {product, handleAddToCart} = props;
+    const { name, img, seller, price, ratings } = product;
+    
     return (
         <div className='product'>
-           <img src={img} alt="" />
-           <div className='product-info'>
-           <p className='product-name'>{name}</p>
-           <p>price:{price} </p>
-           <p> <small>Seller: {seller}</small> </p>
-           <p> <small>Ratings: {ratings} stars </small> </p>
-           </div>
-
-           <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
-            <p>Add to Cart</p>
-           </button>
-           
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
-import './Shop.css'
+import './Shop.css';
 
 const Shop = () => {
-    const [products,setProducts] =useState([]);
+    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
     useEffect( () =>{
@@ -14,29 +14,24 @@ const Shop = () => {
 
     const handleAddToCart = (product) =>{
         console.log(product);
+        // do not do this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
     }
 
-
     return (
-        <div className="shop-container">
+        <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product
-                         key={product.id}
-                         product={product}
-                         handleAddToCart= {handleAddToCart}
-                         
-                         
-                         ></Product>  )
+                    products.map(product=><Product 
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                        ></Product>)
                 }
-             
-
-               
             </div>
             <div className="cart-container">
-                <h3>this is for order summary </h3>
+                <h4>Order Summary</h4>
                 <p>Selected Items: {cart.length}</p>
             </div>
         </div>
