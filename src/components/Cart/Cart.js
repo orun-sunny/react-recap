3336b7268c603b2585568c833d9bc542a4060736
orun sunny <sunny.mukut@gmail.com>
2022-10-05 23:33:22 +0600

    cartAdded

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Cart = () => {
     return (
         <div>
-            
+             <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
         </div>
     );
 };
