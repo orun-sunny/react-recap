da226b7c917184ae8f55d42f3915c442710640e0
orun sunny <sunny.mukut@gmail.com>
2022-10-06 17:30:20 +0600

    innerpart

---
+++ src/components/Inner/Inner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inner = () => {
+    return (
+        <div>
+            <h1>this is inner part
+                
+            </h1>
+        </div>
+    );
+};
+
+export default Inner;
